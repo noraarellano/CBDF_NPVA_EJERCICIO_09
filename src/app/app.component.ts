8bb9459a2bb4011460e881dfe46abafa9d5eea59
noraarellano <72581638+noraarellano@users.noreply.github.com>
2023-11-28 02:41:43 -0600

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,5 +10,23 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'CBDF_NPVA_EJERCICIO_09';
+  title = 'Ejercicio 09 - Interpolacion';
+  nombre = 'Nora Patricia Vicente Arellano';
+  edad = '21';
+  semestre = 'Séptimo semestre';
+  calificaciones = [8, 7, 15, 10, 8, 9, 8, 6];
+  promedio = this.calcularpromedio();
+  estado = this.estadoAlumno();
+
+  calcularpromedio() {
+    let suma = 0;
+    const longuitudArreglo = this.calificaciones.length;
+    for (let i = 0; i < longuitudArreglo; i++) suma += this.calificaciones[i];
+    return suma / longuitudArreglo;
+  }
+
+  estadoAlumno() {
+    if (this.calcularpromedio() >= 6) return 'Aprobado';
+    else return 'No aprobado';
+  }
 }
